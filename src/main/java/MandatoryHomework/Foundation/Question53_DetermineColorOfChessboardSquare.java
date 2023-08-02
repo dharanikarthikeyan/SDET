c3814f0b256c18db1df0247d55ffce055db6c6bc
Admin <Admin@DESKTOP-BFCALPD>
2023-08-02 10:28:45 +0530

    committed yesterday programm

---
+++ src/main/java/MandatoryHomework/Foundation/Question53_DetermineColorOfChessboardSquare.java
@@ -0,0 +1,44 @@
+package MandatoryHomework.Foundation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Question53_DetermineColorOfChessboardSquare {
+
+	/** 1) Split the string into tocharArray()
+	 *  2) Iterate with for loop with the length of char[]
+	 *  3) check for the condition if (ch[i]%2==0 && ch[i+1]%2==0 || ch[i]%2!=0 && ch[i+1]%2!=0)
+	 *  4) if yes return true else return false
+	 * 
+	 */
+	@Test
+	public void test1() {
+		boolean determineColorOfChessboardSquare = determineColorOfChessboardSquare("a1");
+		Assert.assertEquals(false, determineColorOfChessboardSquare);
+	}
+	
+	@Test
+	public void test2() {
+		boolean determineColorOfChessboardSquare = determineColorOfChessboardSquare("A4");
+		Assert.assertEquals(true, determineColorOfChessboardSquare);
+	}
+	@Test
+	public void test3() {
+		boolean determineColorOfChessboardSquare = determineColorOfChessboardSquare("c1");
+		Assert.assertEquals(false, determineColorOfChessboardSquare);
+	}
+	public boolean determineColorOfChessboardSquare(String st) {
+		boolean square=true;
+		char[] ch = st.toCharArray();
+		for (int i = 0; i < ch.length-1; i++) {
+			if(ch[i]%2==0 && ch[i+1]%2==0 || ch[i]%2!=0 && ch[i+1]%2!=0) {
+				square=false;
+				break;
+			}
+		}
+		return square;
+	}
+	
+
+}
+
